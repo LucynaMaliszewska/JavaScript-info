8341979cc9a71133aa46d4f009fed11d14f2e85c
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-05-07 18:32:36 +0200

    create new object

--- test.js
+++ test.js
@@ -1,9 +1,25 @@
-alert("Hello, World");
+var myCar = new Object();
+myCar.maxSpeed = 50;
+myCar.driver = "Shaun";
+myCar.drive = function(){console.log("now driving");
+};
 
-var links = document.getElementsByTagName("a");
+myCar.drive();
 
-for (i=1; i<=links.length; i++) {
-    console.log ("this is link number" + i);
+// krótszy zapis
 
-}
-document.write ("all links now looped");
+var myCar2 = {maxSpeed: 70, driver: "Net Ninja",
+    drive: function(){console.log("now driving");}
+};
+
+// przy określonych wartościach
+
+var myCar2 = {
+    maxSpeed: 70,
+    driver: "Net Ninja",
+    drive: function (speed, time){
+        console.log(speed*time);
+    }
+};
+console.log(myCar2.maxSpeed);
+myCar2.drive(50, 3);
