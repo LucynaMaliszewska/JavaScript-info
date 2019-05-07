72deaf0a50299598418dd578ae18427cd7061b58
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-05-07 20:00:20 +0200

    Example of Constructor Functions added

--- test.js
+++ test.js
@@ -1,11 +1,18 @@
-
-var myCar2 = {
-    maxSpeed: 70, 
-    driver: "Net Ninja", 
-    drive: function(){console.log("now driving");},
-    logDriver: function() {console.log("driver name is" + this.driver)},
-
+var Car = function (maxSpeed,driver){
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function (speed, time){
+        console.log(speed*time);
     };
+    this.logDriver = function(){
+        console.log("driver name is " + this.driver);
+    };
+}
 
+var myCar = new Car (70, "Ninja Net");
+var myCar2 = new Car (40, "Humpty Dumpty");
+var myCar3 = new Car (10, "Sahaun");
+var myCar4 = new Car (90, "James Bond");
 
-myCar2.logDriver();
+myCar.drive (30, 5);
+myCar3.logDriver ();
