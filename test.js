8491c409e6d8af4d361449c406d986d7344674e9
Lucyna Maliszewska <lucyna.malisz@gmail.com>
2019-05-07 19:10:22 +0200

    Keyword THIS added

--- test.js
+++ test.js
@@ -1,25 +1,11 @@
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Shaun";
-myCar.drive = function(){console.log("now driving");
-};
-
-myCar.drive();
-
-// krótszy zapis
-
-var myCar2 = {maxSpeed: 70, driver: "Net Ninja",
-    drive: function(){console.log("now driving");}
-};
-
-// przy określonych wartościach
 
 var myCar2 = {
-    maxSpeed: 70,
-    driver: "Net Ninja",
-    drive: function (speed, time){
-        console.log(speed*time);
-    }
-};
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+    maxSpeed: 70, 
+    driver: "Net Ninja", 
+    drive: function(){console.log("now driving");},
+    logDriver: function() {console.log("driver name is" + this.driver)},
+
+    };
+
+
+myCar2.logDriver();
